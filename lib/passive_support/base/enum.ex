a2040628cf2173cd760a70c342139e6edf589d95
Josh Bourgeois <the.peoples.bourgeois@gmail.com>
2017-09-21 11:54:11 -0700

    Delete `Ps.Enum.length/1`
    It is a more naive, slower method of counting than `Enum.count/1`

--- lib/passive_support/base/enum.ex
+++ lib/passive_support/base/enum.ex
@@ -1,23 +1,5 @@
 defmodule PassiveSupport.Enum do
   @doc """
-  Counts the number of items in the given enumerable.
-
-  Has runtime complexity of O(n), like `Kernel.length/1`, but can be invoked
-  on any structure that implements the `Enumerable` protocol.
-
-  ## Examples
-
-      iex> Ps.Enum.length([1,2,nil])
-      3
-
-      iex> Ps.Enum.length(%{oh: :hi})
-      1
-  """
-  @spec length(Enum.t) :: integer
-  def length(enum), do:
-    Enum.reduce(enum, 0, fn (_item, enum_length) -> enum_length+1 end)
-
-  @doc """
   Initiates a `Task` for each element in the enumerable,
   invoking the provided function, then awaits and returns the processed list.
 
