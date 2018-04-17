27be255e4b3859aa9479c992cc2dadd76fb933c8
Josh Bourgeois <thepeoplesbourgeois+git@gmail.com>
2018-04-16 18:45:54 -0700

    Add doctest for `Ps.Enum.async_map/2`

--- lib/passive_support/base/enum.ex
+++ lib/passive_support/base/enum.ex
@@ -1,7 +1,11 @@
 defmodule PassiveSupport.Enum do
   @doc """
-  Initiates a `Task` for each item in the enumerable,
-  invoking the provided function, then awaits and returns the processed list.
+  Concurrently runs `func` on each item in the enumerable
+
+  ## Examples
+
+      iex> Ps.Enum.async_map(1..11, &(&1 * &1))
+      [1, 4, 9, 16, 25, 36, 49, 64, 81, 100, 121]
   """
   @spec async_map(Enum.t, function) :: Enum.t
   def async_map(enum, func), do:
