303c0b4f9eaab78afe836e5bef80c9f2c080c2dd
Josh Bourgeois <thepeoplesbourgeois+git@gmail.com>
2018-04-19 17:33:33 -0700

    Test more unicode edge cases

--- test/base/string_test.exs
+++ test/base/string_test.exs
@@ -7,8 +7,14 @@ defmodule PassiveSupport.StringTest do
   doctest Ps.String
 
   describe "length_split" do
-    test "preserves the structure of multibyte graphemes" do
+    test "preserves the structure of multipoint graphemes" do
       assert Ps.String.length_split("é", 1, first_split: true) == "é"
+    end
+  end
+
+  describe "length_split when passed a list of lengths" do
+    test "preserves the structure of multibyte graphemes " do
+      assert Ps.String.length_split("æß", [2], first_split: true) == ["æß"]
     end
   end
 end
