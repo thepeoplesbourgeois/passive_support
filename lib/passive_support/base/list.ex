ef4f7743a6ba595cb614fed1429e70346ca6697d
JKB <850082+thepeoplesbourgeois@users.noreply.github.com>
2018-08-14 09:22:13 -0700

    Ps.List.permutations
    Currently returning entries for list sizes > 32 out of order
    due to the particular [implementation of Maps within
    Erlang](https://elixirforum.com/t/performance-of-tuples-over-maps/4591/5).
    
    In the scheme of things, not a huge deal. Might also be helpful in
    spotting better permutations more quickly. But, it's causing the
    spec to fail, so it's "incomplete" at this point.

--- lib/passive_support/base/list.ex
+++ lib/passive_support/base/list.ex
@@ -1,4 +1,6 @@
 defmodule PassiveSupport.List do
+  alias PassiveSupport, as: Ps
+
   @doc ~S"""
   Returns a copy of `list` with any `nil` values removed
 
@@ -47,20 +49,110 @@ defmodule PassiveSupport.List do
   def compact_map([head|tail], fun, options), do:
     [fun.(head) | compact_map(tail, fun, options)]
 
-  #  TODO permutations/1 should return the possible shuffles of the list
-  #  def permutations([h|t]), do: permutations([h|t], length([h|t]))
 
-# BROKEN: Currently returning `[ ["love", "money"] ]` in doctest
-#  @doc ~S"""
-#  Returns all the potential permutations of `sublist_length` for the given list
-#
-#  ## Examples
-#
-#      iex> Ps.List.permutations(["love", "money", "health"], 2)
-#      [["love", "money"], ["love", "health"], ["money", "health"]]
-#  """
-#  def permutations(list, sublist_length)
-#  def permutations([], _), do: []
-#  def permutations(_, 0), do: [[]]
-#  def permutations([element|rest], sublist_length), do: ((for subset <- permutations(rest, sublist_length - 1), do: [element|subset]) ++ permutations(rest, sublist_length))
+  @doc ~S"""
+  Generates a list of all possible permutations of the given list.
+  If `stream: true` is passed,
+
+  ## Examples
+
+      iex> Ps.List.permutations([1,2,3])
+      [
+        [1, 2, 3],
+        [1, 3, 2],
+        [2, 1, 3],
+        [2, 3, 1],
+        [3, 1, 2],
+        [3, 2, 1]
+      ]
+  """
+  # iex> 1..50 |> Enum.to_list |> Ps.List.permutations(stream: true) |> Enum.take(1)
+  # [
+  #   [1,2,3,4,5,6,7,8,9,10,
+  #    11,12,13,14,15,16,17,18,19,20,
+  #    21,22,23,24,25,26,27,28,29,30,
+  #    31,32,33,34,35,36,37,38,39,40,
+  #    41,42,43,44,44,46,47,48,49,50]
+  # ]
+
+
+  @spec permutations([any], [stream: boolean()]) :: [[any]] | Stream.t
+  def permutations(list, options \\ [])
+
+  def permutations([], _), do: [[]]
+  def permutations(list, options) when is_list(list) do
+    lazy = list
+      |> Ps.Enum.to_map
+      |> make_permutations
+    if Keyword.get(options, :stream),
+      do: lazy,
+      else: Enum.to_list(lazy)
+  end
+
+  defp make_permutations(list) do
+    list
+      |> map_permutations(0, 0)
+      # |> list_permutations
+  end
+
+  def list_permutations(list) when is_list(list) do
+    list
+      |> Stream.flat_map(fn next ->
+           Stream.map(list_permutations(list -- [next]), fn(sublist) -> [next | sublist] end)
+         end)
+  end
+
+  defp debug_indent(string, indentation), do: [String.duplicate("  ", indentation), string]
+
+  defp map_permutations(map, _index, _indentation) when map_size(map) == 0, do: [[]]
+  defp map_permutations(map, first_index, debug_level) do
+    first_index..(map_size(map)-1) |> Stream.flat_map(fn _ ->
+      {next, submap} = Map.pop(map, first_index)
+      IO.puts(debug_indent("inside flat_map", debug_level))
+      IO.puts(debug_indent("map: #{inspect(map)}", debug_level))
+      IO.puts(debug_indent("index: #{inspect(first_index)}", debug_level))
+
+      Stream.map(map_permutations(submap, first_index+1, debug_level+1), fn sublist ->
+        IO.puts(debug_indent("sublist: #{inspect(sublist)}", debug_level))
+        [next | sublist]
+      end)
+    end)
+  end
+
+  # @spec map_permutations(%{integer => any()}, integer) :: Stream.t
+  # defp map_permutations(map, debug_indentation) do
+  #   indent = fn string -> [String.duplicate("  ", debug_indentation*2), string] end
+  #   # IO.puts(indent.("inside map_permutations #{debug_indentation}"))
+  #   Stream.flat_map(0..map_size(map), fn index ->
+  #     # IO.puts(indent.("  next: #{inspect(next)}"))
+  #     next = map[index]
+  #     Stream.map(
+  #       map_permutations(Map.delete(map, index), debug_indentation+1),
+  #       fn sublist ->
+  #         IO.puts(indent.("  sublist: #{inspect(sublist)}"))
+  #         [next | sublist]
+  #       end
+  #     )
+  #   end)
+  # end
+        # if map_size(submap) in 1..2 do
+        #   require IEx
+        #   IEx.pry
+        # end
+
+        # case map_size(submap) == 0 do
+          # true ->
+          #     require IEx
+          #     IEx.pry
+          #   [following | []]
+          # false ->
+            #   require IEx
+            #   IEx.pry
+            # [ following | map_permutations(submap, (debug_indentation+1)) ]
+        # end
+      # end)
+      # IO.puts(indent.("  sublist: #{inspect(sublist)}"))
+      # [next | sublist]
+    # end)
+  # end
 end
