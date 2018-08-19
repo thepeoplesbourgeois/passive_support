640bee12ee797ad73fdc3632bad3dea597b972bc
JKB <850082+thepeoplesbourgeois@users.noreply.github.com>
2018-08-18 19:21:08 -0700

    Update version info

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule PassiveSupport.Mixfile do
 
   def project do
     [app: :passive_support,
-     version: "0.1.0",
-     elixir: "~> 1.4",
+     version: "0.4.0",
+     elixir: "~> 1.6",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
