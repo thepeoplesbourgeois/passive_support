9bdea8b87f24a117c198bf67b35751b4ec706133
JKB <850082+thepeoplesbourgeois@users.noreply.github.com>
2018-08-25 11:05:05 -0700

    Documentation for PassiveSupport.Integer

--- lib/passive_support/base/integer.ex
+++ lib/passive_support/base/integer.ex
@@ -1,18 +1,43 @@
 defmodule PassiveSupport.Integer do
+  @doc """
+  Qualifies if `integer` is an integer less than 0.
+  """
   defguard is_negative(integer)
     when is_integer(integer)
     and integer < 0
 
+  @doc """
+  Qualifies if `integer` is a natural number or 0.
+  """
+  defguard is_nonnegative(integer)
+    when is_integer(integer)
+    and integer >= 0
+
+  @doc """
+  Qualifies if `integer` is an integer greater than 0.
+
+  (adhering to the mathematical principle that 0 is neither positive nor negative)
+  """
+  defguard is_positive(integer)
+    when is_integer(integer)
+    and integer > 0
+
+  @doc """
+  Qualifies if `integer` is even.
+  """
   defguard is_even(integer)
     when is_integer(integer)
     and rem(integer, 2) == 0
 
+  @doc """
+  Qualifies if `integer` is odd.
+  """
   defguard is_odd(integer)
     when is_integer(integer)
     and rem(integer, 2) == 1
 
   @doc ~S"""
-  Returns the factorial of `n`
+  Arbitrary-precision factorial.
 
   ## Examples
 
@@ -37,9 +62,7 @@ defmodule PassiveSupport.Integer do
   defp do_fact(next, factored), do: do_fact(next-1, factored * next)
 
   @doc ~S"""
-  Returns an arbitrary-precision integer representation of `base^factor`,
-  which must be integers. This contrasts to the behavior of `:math.pow/2`,
-  which accepts floats, and consistently returns a float.
+  Arbitrary-precision exponentiation
 
   Original implementation from https://stackoverflow.com/questions/32024156/how-do-i-raise-a-number-to-a-power-in-elixir#answer-32030190
 
@@ -53,12 +76,16 @@ defmodule PassiveSupport.Integer do
 
       iex> Ps.Integer.exponential(2, 100)
       1267650600228229401496703205376
+
+      iex> Ps.Integer.exponential(2, -2)
+      0.25
   """
   # TODO: tail-call optimize
   @spec exponential(integer, integer) :: integer
 
   def exponential(_, 0), do:
     1
+  def exponential(base, factor) when is_negative(factor), do: 1 / (exponential(base, -factor))
   def exponential(base, factor) when is_odd(factor) do
     recursed_factor = exponential(base, factor - 1)
     base * recursed_factor
@@ -87,5 +114,31 @@ defmodule PassiveSupport.Integer do
   #  def  exp(n, k), do: exp(n, k, 1)
   #  defp exp(_, 0, acc), do: acc
   #  defp exp(n, k, acc), do: exp(n, k - 1, n * acc)
+
+  @doc """
+  Converts an integer to a string, with a `separator` (default `","`)
+  inserted, starting in from the right side of the number, spaced apart
+  by the number of digits specified by `spacing` (default `3`).
+
+  ## Examples
+
+      iex> PassiveSupport.Integer.human_readable(57468291379)
+      "57,468,291,379"
+      iex> PassiveSupport.Integer.human_readable(57468291379, separator: "_")
+      "57_468_291_379"
+      iex> PassiveSupport.Integer.human_readable(57468291379, spacing: 4)
+      "574,6829,1379"
+  """
+  def human_readable(integer, opts \\ []) do
+    separator = Keyword.get(opts, :separator, ",")
+    spacing = Keyword.get(opts, :spacing, 3)
+    integer
+      |> Integer.digits
+      |> Stream.map(&to_string/1)
+      |> Enum.reverse
+      |> Enum.chunk_every(spacing)
+      |> Enum.join(separator)
+      |> String.reverse
+  end
 
 end
