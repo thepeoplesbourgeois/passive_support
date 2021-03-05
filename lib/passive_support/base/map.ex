0c013143d7069a2baf88188a9703bdeca099fd67
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2021-03-04 21:34:36 -0800

    Improve wording in documentation

--- lib/passive_support/base/map.ex
+++ lib/passive_support/base/map.ex
@@ -1,56 +1,58 @@
 defmodule PassiveSupport.Map do
+  @moduledoc """
+  Convenience functions for working with maps.
+  """
 
   @doc ~S"""
-    Returns a version of `map` whose keys have all been
-    converted to strings.
+  Returns a version of `map` whose keys have all been
+  converted to strings.
 
-    ## Examples
+  ## Examples
 
-        iex> stringify_keys(%{7 => 8, "hey" => "ha", hi: "ho"})
-        %{"7" => 8, "hey" => "ha", "hi" => "ho"}
-    """
-  def stringify_keys(%{} = map), do:
-    map
-      |> Stream.map(fn {key, value} -> {to_string(key), value} end)
-      |> Enum.into(%{})
-
-  @doc ~S"""
-    Returns a version of `map` whose keys have all been
-    converted to atoms, first by being coerced to a string,
-    then being run through `String.to_existing_atom/1`
-
-    ## Examples
-
-        iex> [:oh, :yay] # existing keys
-        iex> atomize_keys!(%{"oh" => "ooh", 'yay' => 'yaaay'})
-        %{oh: "ooh", yay: 'yaaay'}
-        iex> atomize_keys!(%{"oh" => "ooh", "noo" => "noooo"})
-        ** (ArgumentError) argument error
-    """
-  def atomize_keys!(%{} = map), do:
-    map
-      |> Stream.map(fn {key, value} ->
-           atom = key |> to_string |> String.to_existing_atom
-           {atom, value}
-         end)
-      |> Enum.into(%{})
+      iex> stringify_keys(%{7 => 8, "hey" => "ha", hi: "ho"})
+      %{"7" => 8, "hey" => "ha", "hi" => "ho"}
+  """
+  def stringify_keys(%{} = map),
+    do: map
+     |> Stream.map(fn {key, value} -> {to_string(key), value} end)
+     |> Enum.into(%{})
 
   @doc ~S"""
-    Returns a version of `map` whose keys that can be
-    coerced to existing atoms are converted thusly
+  Returns `map` with its keys as atoms, if those atoms already exist.
 
-    I'm not giving y'all the ridiculous footgun
-    of wanton atom space pollution. I'm not some foot-shooting
-    cowboy like `Jason`.
+  Throws an exception otherwise
 
-    ## Examples
+  ## Examples
 
-        iex> [:oh, :yay] # existing keys
-        iex> safe_atomize_keys(%{"oh" => "ooh", 'yay' => 'yaaay'})
-        %{oh: "ooh", yay: 'yaaay'}
-        iex> safe_atomize_keys(%{"oh" => "ooh", "noo" => "noooo"})
-        %{oh: "ooh"}
-    """
+      iex> [:oh, :yay] # existing keys
+      iex> atomize_keys!(%{"oh" => "ooh", 'yay' => 'yaaay'})
+      %{oh: "ooh", yay: 'yaaay'}
+      iex> atomize_keys!(%{"oh" => "ooh", "noo" => "noooo"})
+      ** (ArgumentError) argument error
+  """
+  def atomize_keys!(%{} = map),
+    do: map
+     |> Stream.map(fn {key, value} ->
+          atom = key |> to_string |> String.to_existing_atom
+          {atom, value}
+        end)
+     |> Enum.into(%{})
+
+  @doc ~S"""
+  Returns a `map` with any string keys that safely coerced to existing atoms
+
+  I'm not giving y'all the ridiculous footgun of
+  wanton atom space pollution. I'm not some foot-shooting
+  cowboy like `Jason`.
+
+  ## Examples
+
+      iex> [:oh, :yay] # existing keys
+      iex> safe_atomize_keys(%{"oh" => "ooh", 'yay' => 'yaaay'})
+      %{oh: "ooh", yay: 'yaaay'}
+      iex> safe_atomize_keys(%{"oh" => "ooh", "noo" => "noooo"})
+      %{oh: "ooh"}
+  """
   def safe_atomize_keys(%{} = map),
     do: map
      |> Stream.map(fn {key, value} -> {to_string(key), value} end)
