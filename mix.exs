449c14eae7eb890bef612071505ee026acb1a1c6
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2021-04-07 06:58:04 -0700

    Update required Elixir version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PassiveSupport.Mixfile do
   def project do
     [app: :passive_support,
      version: "0.5.1",
-     elixir: "~> 1.6",
+     elixir: "~> 1.9",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps(),
