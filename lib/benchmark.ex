2e6e6ec8871f3a907a86f4b3e58f0c338cf8746e
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2021-04-19 01:02:32 -0700

    Results for 1/2 the functions in question
    Also, the benchmark

---
+++ lib/benchmark.ex
@@ -0,0 +1,23 @@
+defmodule Benchmark do
+  @time 15
+  # @memory @time / 2
+  @warmup 3
+  alias PassiveSupport.Integer, as: Int
+  def maths, do: Benchee.run(%{
+    "iterative" => fn {base, exp} -> Int.exponential(base, exp) end,
+    # "recursive" => fn {base, exp} -> Int.pow(base, exp) end
+  },
+  inputs:
+    for [base, xs] <- [[2, 15..19], [3, 9..12], [5, 4..8], [7, 2..6], [11, 2..5]],
+      xpnt <- xs do
+      {base, xpnt}
+    end
+     |> Stream.with_index(1)
+     |> Stream.map(fn {{base, exp}, index} ->
+          {"#{index}: #{base}^(#{base}^#{exp})", {base, Int.exponential(base, exp)}}
+        end)
+     |> Enum.to_list,
+  time: @time,
+  warmup: @warmup
+  )
+end
