cbe1d75510c4397c9e04234fa5560e3fd99a0c49
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2021-04-20 02:45:34 -0700

    Benchmarkable variations on integer exponentiation
    To benchmark them, simply call `Benchmark.maths/0` from IEx.

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -1,23 +1,25 @@
 defmodule Benchmark do
-  @time 15
-  @warmup 3
+  @time 8
+  @warmup 2
   alias PassiveSupport.Integer, as: Int
 
   def maths, do: Benchee.run(%{
-    "tail-call recursive" => fn {base, exp} -> Int.exponential(base, exp) end,
-    # "recursive" => fn {base, exp} -> Int.pow(base, exp) end
+    "A. body-recursive" => fn {base, exp} -> Int.pow(base, exp) end,
+    "B. iterative" => fn {base, exp} -> Int.iter_exponential(base, exp) end,
+    "C. tail-recursive" => fn {base, exp} -> Int.tail_exponential(base, exp) end
   },
   inputs:
-    for [base, xs] <- [[2, 15..19], [3, 9..12], [5, 4..8], [7, 2..6], [11, 2..5]],
+    for [base, xs] <- [[2, 17..19], [3, 10..12], [5, 6..8], [7, 3..6], [11, 3..5]],
       xpnt <- xs do
       {base, xpnt}
     end
      |> Stream.with_index(1)
      |> Stream.map(fn {{base, exp}, index} ->
-          {"#{index}: #{base}^(#{base}^#{exp})", {base, Int.exponential(base, exp)}}
+          {"#{index}: #{base}^(#{base}^#{exp})", {base, Int.iter_exponential(base, exp)}}
         end)
      |> Enum.to_list,
   time: @time,
-  warmup: @warmup
+  warmup: @warmup,
+  save: %{path: "exponanza_" <> (DateTime.utc_now |> DateTime.to_iso8601(:basic) |> String.split(".") |> hd) <> ".benchee"}
   )
 end
