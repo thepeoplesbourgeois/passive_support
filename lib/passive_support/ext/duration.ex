108fc6b85608b10e3422a882cf39537c6120dfd3
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2021-04-29 21:01:00 -0700

    Revert repetitive motion changes

--- lib/passive_support/ext/duration.ex
+++ lib/passive_support/ext/duration.ex
@@ -1,6 +1,6 @@
 defmodule PassiveSupport.Duration do
-  alias __MODULE__, as: D
-  @type t :: %D{}
+  alias __MODULE__
+  @type t :: %Duration{}
   defstruct seconds: 0, minutes: 0, hours: 0, days: 0, weeks: 0, months: 0, years: 0
 
   @doc """
@@ -8,8 +8,8 @@ defmodule PassiveSupport.Duration do
 
 
   """
-  @spec to_milliseconds(D.t) :: integer
-  def to_milliseconds(%D{} = d) do
+  @spec to_milliseconds(Duration.t) :: integer
+  def to_milliseconds(%Duration{} = d) do
     millis(d.seconds, :seconds) +
     millis(d.minutes, :minutes) +
     millis(d.hours, :hours) +
@@ -29,30 +29,30 @@ defmodule PassiveSupport.Duration do
   defp millis(time, :years), do: millis(time * 12, :months)
 
   @spec seconds(t, number) :: t
-  def seconds(t \\ %D{}, time)
-  def seconds(%D{seconds: s} = t, time), do: %{ t | seconds: s + time}
+  def seconds(t \\ %Duration{}, time)
+  def seconds(%Duration{seconds: s} = t, time), do: %{ t | seconds: s + time}
 
   @spec minutes(t, number) :: t
-  def minutes(t \\ %D{}, time)
-  def minutes(%D{minutes: m} = t, time), do: %{ t | minutes: m + time }
+  def minutes(t \\ %Duration{}, time)
+  def minutes(%Duration{minutes: m} = t, time), do: %{ t | minutes: m + time }
 
   @spec hours(t, number) :: t
-  def hours(t \\ %D{}, time)
-  def hours(%D{hours: h} = t, time), do: %{ t | hours: h + time }
+  def hours(t \\ %Duration{}, time)
+  def hours(%Duration{hours: h} = t, time), do: %{ t | hours: h + time }
 
   @spec days(t, number) :: t
-  def days(t \\ %D{}, time)
-  def days(%D{days: d} = t, time), do: %{ t | days: d + time }
+  def days(t \\ %Duration{}, time)
+  def days(%Duration{days: d} = t, time), do: %{ t | days: d + time }
 
   @spec weeks(t, number) :: t
-  def weeks(t \\ %D{}, time)
-  def weeks(%D{weeks: w} = t, time), do: %{ t | weeks: w + time }
+  def weeks(t \\ %Duration{}, time)
+  def weeks(%Duration{weeks: w} = t, time), do: %{ t | weeks: w + time }
 
   @spec months(t, number) :: t
-  def months(t \\ %D{}, time)
-  def months(%D{months: m} = t, time), do: %{ t | months: m + time }
+  def months(t \\ %Duration{}, time)
+  def months(%Duration{months: m} = t, time), do: %{ t | months: m + time }
 
   @spec years(t, number) :: t
-  def years(t \\ %D{}, time)
-  def years(%D{years: y} = t, time), do: %{ t | years: y + time }
+  def years(t \\ %Duration{}, time)
+  def years(%Duration{years: y} = t, time), do: %{ t | years: y + time }
 end
