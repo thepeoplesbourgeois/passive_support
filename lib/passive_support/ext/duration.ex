0c0c79886e7c8df39a2b7e72cfebd0906bcf4e7e
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2021-05-19 20:23:36 -0700

    Doctest the thing

--- lib/passive_support/ext/duration.ex
+++ lib/passive_support/ext/duration.ex
@@ -3,31 +3,6 @@ defmodule PassiveSupport.Duration do
   @type t :: %Duration{}
   defstruct seconds: 0, minutes: 0, hours: 0, days: 0, weeks: 0, months: 0, years: 0
 
-  @doc """
-  Returns the raw number of milliseconds represented by the duration.
-
-
-  """
-  @spec to_milliseconds(Duration.t) :: integer
-  def to_milliseconds(%Duration{} = d) do
-    millis(d.seconds, :seconds) +
-    millis(d.minutes, :minutes) +
-    millis(d.hours, :hours) +
-    millis(d.days, :days) +
-    millis(d.weeks, :weeks) +
-    millis(d.months, :months) +
-    millis(d.years, :years)
-  end
-
-  defp millis(time, :seconds), do: time * 1000
-  defp millis(time, :minutes), do: millis(time * 60, :seconds)
-  defp millis(time, :hours), do: millis(time * 60, :minutes)
-  defp millis(time, :days), do: millis(time * 24, :hours)
-  defp millis(time, :weeks), do: millis(time * 7, :days)
-  # 365 days / 7 per week / 12 months = 4.3452381 weeks per average month
-  defp millis(time, :months), do: round(millis(time * 4.3452381, :weeks))
-  defp millis(time, :years), do: millis(time * 12, :months)
-
   @spec seconds(t, number) :: t
   def seconds(t \\ %Duration{}, time)
   def seconds(%Duration{seconds: s} = t, time), do: %{ t | seconds: s + time}
@@ -55,6 +30,58 @@ defmodule PassiveSupport.Duration do
   @spec years(t, number) :: t
   def years(t \\ %Duration{}, time)
   def years(%Duration{years: y} = t, time), do: %{ t | years: y + time }
+
+  @doc """
+  Returns the raw representation of seconds captured by the duration.
+
+  ## Examples
+
+      iex> minutes(10) |> hours(4) |> to_seconds
+      15000
+
+      iex> years(1) |> to_seconds
+      525600 * 60
+  """
+  def to_seconds(%Duration{} = d) do
+    d.seconds +
+    in_seconds(d.minutes, :minutes) +
+    in_seconds(d.hours, :hours) +
+    in_seconds(d.days, :days) +
+    in_seconds(d.weeks, :weeks) +
+    in_seconds(d.months, :months) +
+    in_seconds(d.years, :years)
+  end
+
+  defp in_seconds(time, :seconds), do: time
+  defp in_seconds(time, :minutes), do: in_seconds(time * 60, :seconds)
+  defp in_seconds(time, :hours), do: in_seconds(time * 60, :minutes)
+  defp in_seconds(time, :days), do: in_seconds(time * 24, :hours)
+  defp in_seconds(time, :weeks), do: in_seconds(time * 7, :days)
+  # 365 days / 7 per week / 12 months = 4.3452381 weeks per average month
+  defp in_seconds(time, :months), do: round(in_seconds(time * 4.3452381, :weeks))
+  defp in_seconds(time, :years), do: in_seconds(time * 12, :months)
+
+  @doc """
+  Returns the raw number of milliseconds represented by the duration.
+
+  ## Examples
+
+      iex> minutes(10) |> hours(4) |> to_milliseconds
+      15000000
+  """
+  @spec to_milliseconds(Duration.t) :: integer
+  def to_milliseconds(%Duration{} = d) do
+    (d.seconds +
+    in_seconds(d.minutes, :minutes) +
+    in_seconds(d.hours, :hours) +
+    in_seconds(d.days, :days) +
+    in_seconds(d.weeks, :weeks) +
+    in_seconds(d.months, :months) +
+    in_seconds(d.years, :years)) * 1000
+  end
+
+  defp millis(time), do: time * 1000
+  defp millis(time, not_seconds), do: in_seconds(time, not_seconds) |> millis
 
   defimpl String.Chars do
     def to_string(duration) do
---
+++ test/base/path/duration_test.exs
@@ -0,0 +1,4 @@
+defmodule PassiveSupport.DurationTest do
+  use ExUnit.Case, async: true
+  doctest PassiveSupport.Duration, import: true
+end
