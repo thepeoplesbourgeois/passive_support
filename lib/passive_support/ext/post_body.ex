29dcf0b7ef7c431c43e174f5e53baed59d72326b
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2021-07-24 04:33:15 -0700

    The `www_form_urlencoded` nightmare begins

---
+++ lib/passive_support/ext/post_body.ex
@@ -0,0 +1,77 @@
+defmodule PassiveSupport.PostBody do
+  @doc ~s"""
+  Deep-parses the map into a string formatted according to the `www-form-urlencoded` spec.
+
+  ## Examples
+
+      iex> parse_form_data(%{
+      ...>   "success_url" => "https://localhost:4001/membership/subscribe?session_id={CHECKOUT_SESSION_ID}",
+      ...>   "cancel_url" => "https://localhost:4001/membership/nevermind",
+      ...>   "payment_types" => ["grass", "gas", "ass"],
+      ...>   "line_items" => [%{"price" => "five dollars", "quantity" => 1}, %{"line" => "line", "item" => "item"}],
+      ...>   "mode" => "subscription",
+      ...>   "just_to_make_sure" => %{
+      ...>     "we got" => "all", "the edge" => "cases", "we can think of" => "covered"
+      ...>   }
+      ...> })
+      ~S(#{
+        }cancel_url="https://localhost:4001/membership/nevermind"&#{
+        }just_to_make_sure[the+edge]=cases&#{
+        }just_to_make_sure[we+can+think+of]=covered&#{
+        }just_to_make_sure[we+got]=all&#{
+        }line_items[0][price]=five+dollars&#{
+        }line_items[0][quantity]=1&#{
+        }line_items[1][item]=item&#{
+        }line_items[1][line]=line&#{
+        }mode=subscription&#{
+        }payment_types[0]=grass&#{
+        }payment_types[1]=gas&#{
+        }payment_types[2]=ass&#{
+        }success_url="https://localhost:4001/membership/subscribe?session_id={CHECKOUT_SESSION_ID}"#{
+      })
+  """
+  @spec parse_form_data(map) :: String.t
+  def parse_form_data(enum) when is_map(enum), do: do_parse_form_data(enum)
+  defp do_parse_form_data(enum, datum_path \\ [])
+  defp do_parse_form_data(enum, datum_path) when is_list(enum) do
+    enum
+     |> Stream.with_index
+     |> Enum.map(fn
+          {value, index} ->
+            if Enumerable.impl_for(value),
+              do: do_parse_form_data(value, [index | datum_path]),
+              else: [construct_path([index | datum_path]), sanitize_input(value)] |> Enum.join("=")
+        end)
+     |> Enum.join("&")
+     |> IO.inspect(label: "list data")
+  end
+  defp do_parse_form_data(enum, datum_path) do
+    enum
+     |> Enum.map(fn
+          {key, value} ->
+            if Enumerable.impl_for(value),
+              do: do_parse_form_data(value, [key | datum_path]),
+              else: [construct_path([key | datum_path]), sanitize_input(value)] |> Enum.join("=")
+        end)
+     |> Enum.join("&")
+     |> IO.inspect(label: "map data")
+  end
+
+  defp construct_path(path, path_iolist \\ [])
+  defp construct_path([key | []], path_iolist),
+    do: key
+     |> sanitize_input
+     |> IO.inspect(label: "path_root")
+     |> then(&[&1 | path_iolist])
+     |> IO.chardata_to_string()
+  defp construct_path([key_or_index | path], path_iolist),
+    do: construct_path(path, [["[" , sanitize_input(key_or_index), "]"], path_iolist])
+
+  defp sanitize_input(index) when is_integer(index), do: to_string(index)
+  defp sanitize_input("" <> input) do
+    case String.match?(input, ~r"[/&?]") do
+      true -> input |> URI.parse |> to_string |> inspect
+      false -> URI.encode_www_form(input)
+    end
+  end
+end
---
+++ test/ext/post_body_test.exs
@@ -0,0 +1,4 @@
+defmodule PassiveSupport.PostBodyTest do
+  use ExUnit.Case, async: true
+  doctest PassiveSupport.PostBody, import: true
+end
