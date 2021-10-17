12f3c2a316719b4610cbe34c28f007dbe1fd7d35
The Peoples' Bourgeois <850082+thepeoplesbourgeois@users.noreply.github.com>
2021-10-16 18:57:16 -0700

    `parse_form_data` -> `form_encoded`

--- lib/passive_support/ext/post_body.ex
+++ lib/passive_support/ext/post_body.ex
@@ -8,65 +8,75 @@ defmodule PassiveSupport.PostBody do
 
   ## Examples
 
-      iex> parse_form_data(%{
+      iex> form_encoded(%{
       ...>   "success_url" => "https://localhost:4001/foo/bert?id={ID}",
       ...>   "cancel_url" => "https://localhost:4001/foo/bar",
-      ...>   "payment_types" => ["cash", "card", "cheque"],
-      ...>   "line_items" => [%{"price" => "five dollars", "quantity" => 1}, %{"line" => "line", "item" => "item"}],
-      ...>   "mode" => "subscription",
+      ...>   "blips" => ["blop", "blorp", "blep"],
+      ...>   "array" => [
+      ...>     %{"something" => "five dollars", "quantity" => 1},
+      ...>     %{"line" => "line", "item" => "item"},
+      ...>     %{"let's" => ~W[really flerf this up]}
+      ...>   ],
+      ...>   "mode" => "median",
       ...>   "just_to_make_sure" => %{
       ...>     "we got" => "all", "the edge" => "cases", "we can think of" => "covered"
       ...>   }
       ...> })
-      ~S(cancel_url="https://localhost:4001/foo/bar"&#{
+      ~S(array[0][quantity]=1&#{
+        }array[0][something]=five+dollars&#{
+        }array[1][item]=item&#{
+        }array[1][line]=line&#{
+        }array[2][let%27s][0]=really&#{
+        }array[2][let%27s][1]=flerf&#{
+        }array[2][let%27s][2]=this&#{
+        }array[2][let%27s][3]=up&#{
+        }blips[0]=blop&#{
+        }blips[1]=blorp&#{
+        }blips[2]=blep&#{
+        }cancel_url="https://localhost:4001/foo/bar"&#{
         }just_to_make_sure[the+edge]=cases&#{
         }just_to_make_sure[we+can+think+of]=covered&#{
         }just_to_make_sure[we+got]=all&#{
-        }line_items[0][price]=five+dollars&#{
-        }line_items[0][quantity]=1&#{
-        }line_items[1][item]=item&#{
-        }line_items[1][line]=line&#{
-        }mode=subscription&#{
-        }payment_types[0]=cash&#{
-        }payment_types[1]=card&#{
-        }payment_types[2]=cheque&#{
+        }mode=median&#{
         }success_url="https://localhost:4001/foo/bert?id={ID}"#{
       })
 
-      iex> parse_form_data(%{something: :dotcom})
+      iex> form_encoded(%{something: :dotcom})
       "something=dotcom"
   """
-  @spec parse_form_data(map) :: String.t
-  def parse_form_data([{_,_}|_] = enum), do: do_parse_form_data(enum)
-  def parse_form_data(enum) when is_map(enum), do: do_parse_form_data(enum)
-  defp do_parse_form_data(enum, data_path \\ [])
-  defp do_parse_form_data([{_,_}|_] = enum, data_path) do
+  @spec form_encoded(map) :: String.t
+  # Need to differentiate keyword lists, keywordish lists, and maps from generic lists;
+  # the former can all be form encoded, generic lists cannot be.
+  def form_encoded([{_,_}|_] = enum), do: do_form_encoding(enum)
+  def form_encoded(enum) when is_map(enum), do: do_form_encoding(enum)
+  defp do_form_encoding(enum, data_path \\ [])
+  defp do_form_encoding([{_,_}|_] = enum, data_path) do
     enum
      |> Enum.map(fn
           {key, value} ->
             if Enumerable.impl_for(value),
-              do: do_parse_form_data(value, [key | data_path]),
+              do: do_form_encoding(value, [key | data_path]),
               else: [construct_path([key | data_path]), sanitize_input(value)] |> Enum.join("=")
         end)
      |> Enum.join("&")
   end
-  defp do_parse_form_data(%{} = enum, data_path) do
+  defp do_form_encoding(%{} = enum, data_path) do
     enum
      |> Enum.map(fn
           {key, value} ->
             if Enumerable.impl_for(value),
-              do: do_parse_form_data(value, [key | data_path]),
+              do: do_form_encoding(value, [key | data_path]),
               else: [construct_path([key | data_path]), sanitize_input(value)] |> Enum.join("=")
         end)
      |> Enum.join("&")
   end
-  defp do_parse_form_data([_|_] = enum, data_path) do
+  defp do_form_encoding([_|_] = enum, data_path) do
     enum
      |> Stream.with_index
      |> Enum.map(fn
           {value, index} ->
             if Enumerable.impl_for(value),
-              do: do_parse_form_data(value, [index | data_path]),
+              do: do_form_encoding(value, [index | data_path]),
               else: [construct_path([index | data_path]), sanitize_input(value)] |> Enum.join("=")
         end)
      |> Enum.join("&")
